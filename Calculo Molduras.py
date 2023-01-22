9de950f8fd8f6b6cd7fddd71e7cf792702739642
Mliguili <marcel.liguili@gmail.com>
2023-01-21 23:43:38 -0300

    Lógica funcional sem contar o numero de quadros

--- Calculo Molduras.py
+++ Calculo Molduras.py
@@ -1,18 +1,23 @@
 l = 30
 a = 21
 num_quadros = 188
-area_vidro = l * a
-l_vidro = 240
-a_vidro = 160
 barra = 270
 l_barra = 2
 desconto = (l_barra*2) + 1
 if l > a:
-    perna_gnd = l
-    perna_peq = a
+    perna_gnd = l + desconto
+    perna_peq = a + desconto
 else:
-    perna_gnd = a
-    perna_peq = l
+    perna_gnd = a + desconto
+    perna_peq = l + desconto
 
-quant_perna_gnd = (barra/ (perna_gnd + desconto)).__int__() * num_quadros
-print(quant_perna_gnd)
+num_per_gnd = int(barra / perna_gnd)
+sobra = barra - (num_per_gnd * perna_gnd)
+
+if sobra >= (perna_peq - desconto):
+    num_per_peq_sobra = int(sobra / (perna_peq - desconto))
+    nova_sobra = sobra - (num_per_peq_sobra * (perna_peq - desconto))
+    print(f"pra esse projeto, terá {num_per_gnd} perna(s) grandes e {num_per_peq_sobra} perna(s) pequena por barra e {nova_sobra} cm de sobra".upper())
+
+else:
+    print(f"pra esse projeto, terá {num_per_gnd} perna(s) grandes e {sobra} cm de sobra".upper())
