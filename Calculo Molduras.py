425becea1e0848fc0fa5dba46314851b2eccd055
Mliguili <marcel.liguili@gmail.com>
2023-01-22 22:35:26 -0300

    ainda em construção

--- Calculo Molduras.py
+++ Calculo Molduras.py
@@ -1,7 +1,7 @@
 l = 30
 a = 21
 num_quadros = 188
-barra = 270
+barra = 275
 l_barra = 2
 desconto = (l_barra*2) + 1
 if l > a:
@@ -11,13 +11,30 @@ else:
     perna_gnd = a + desconto
     perna_peq = l + desconto
 
-num_per_gnd = int(barra / perna_gnd)
-sobra = barra - (num_per_gnd * perna_gnd)
+num_per_gnd_por_barra = int(barra / perna_gnd)
+sobra = barra - (num_per_gnd_por_barra * perna_gnd)
+
+total_perna_gnd_por_porjeto = num_quadros * 2
+total_perna_peq_por_porjeto = num_quadros * 2
+
+
 
 if sobra >= (perna_peq - desconto):
     num_per_peq_sobra = int(sobra / (perna_peq - desconto))
     nova_sobra = sobra - (num_per_peq_sobra * (perna_peq - desconto))
-    print(f"pra esse projeto, terá {num_per_gnd} perna(s) grandes e {num_per_peq_sobra} perna(s) pequena por barra e {nova_sobra} cm de sobra".upper())
+    print(f"por barra terá {num_per_gnd_por_barra} perna(s) grandes e {num_per_peq_sobra} perna(s) pequena por barra e {nova_sobra} cm de sobra".upper())
+    print(f" Total de pernas grandes: {total_perna_gnd_por_porjeto}".upper())
+    print(f" Total de pernas pequenas: {total_perna_gnd_por_porjeto}".upper())
 
 else:
-    print(f"pra esse projeto, terá {num_per_gnd} perna(s) grandes e {sobra} cm de sobra".upper())
+    print(f"por barra terá {num_per_gnd_por_barra} perna(s) grandes e {sobra} cm de sobra".upper())
+    print(f" Total de pernas grandes: {total_perna_gnd_por_porjeto}".upper())
+    print(f" Total de pernas pequenas: {total_perna_gnd_por_porjeto}".upper())
+
+#num_barras_para_per_gnd = total_perna_gnd_por_porjeto/num_per_gnd_por_barra
+#num_barras_para_per_peq = total_perna_peq_por_porjeto/num_per_peq_sobra
+
+#if num_quadros != 1:
+    #print(f'precisa de {num_barras_para_per_gnd} barras para as pernas grandes'.upper())
+    #print(f'precisa de {num_barras_para_per_peq} barras para as pernas pequenas'.upper())
+
